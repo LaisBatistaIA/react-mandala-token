6bf8410e7d3b125260cd9c1fad185aaa5128f6c4
Lais Batista <lais_batista@atlantico.com.br>
2024-12-04 09:24:40 -0300

    🎉 feat: first commit.

---
+++ src/common/languages.jsx
@@ -0,0 +1,20 @@
+import usFlag from "../assets/images/flags/us.jpg"
+import spain from "../assets/images/flags/spain.jpg"
+import brazil from "../assets/images/flags/brazil.jpg"
+
+const languages = {
+  sp: {
+    label: "Spanish",
+    flag: spain,
+  },
+  pt: {
+    label: "Portugues",
+    flag: brazil,
+  },
+  en: {
+    label: "English",
+    flag: usFlag,
+  },
+}
+
+export default languages
